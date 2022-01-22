fa00041167b55f14fdfe4f75d44e8eeb77eadc38
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-22 19:03:01 +0500

    cdk app

---
+++ step20_step_functions/step04_parallel/test/step04_parallel.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Step04Parallel from '../lib/step04_parallel-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step04_parallel-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step04Parallel.Step04ParallelStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
