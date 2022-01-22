8f162635c7ba2d5c3d182a897d551201b4f7806a
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-22 19:30:14 +0500

    Update step05_map-stack.ts

--- step20_step_functions/step05_map/lib/step05_map-stack.ts
+++ step20_step_functions/step05_map/lib/step05_map-stack.ts
@@ -1,15 +1,36 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as stepFunctions from '@aws-cdk/aws-stepfunctions';
 
 export class Step05MapStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // this initial "pass" function inserts an array in the output {}
+    const initialState = new stepFunctions.Pass(this, 'initialState', {
+      result: stepFunctions.Result.fromArray(['entry1', 'entry2', 'entry3']),
+      resultPath: '$.arrayOutput',
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Step05MapQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // this pass function doesnt change anything. We just made this to explain the mapping process
+    const mapPass = new stepFunctions.Pass(this, 'mapping');
+
+    // this is a mapping function that runs 'mapPass' for each element in the state path {arrayOutput:[....]} coming
+    // from the previous step. In this case we have 3 elements in the array therefore the "mapPass" would run 3 times.
+    const map = new stepFunctions.Map(this, 'MapState', {
+      maxConcurrency: 1,
+      itemsPath: stepFunctions.JsonPath.stringAt('$.arrayOutput'),
+    });
+    map.iterator(mapPass);
+
+    // created a chain
+    const chain = stepFunctions.Chain.start(initialState).next(map);
+    // create a state machine
+    new stepFunctions.StateMachine(this, 'mapStateMachine', {
+      definition: chain,
+    });
   }
 }
+//
+
+//   }
+// }
