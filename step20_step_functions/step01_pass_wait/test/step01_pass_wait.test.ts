06cc726c0ecc9132d2037ba29c9322d50434ee84
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-22 18:12:53 +0500

    cdk app

---
+++ step20_step_functions/step01_pass_wait/test/step01_pass_wait.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as Step01PassWait from '../lib/step01_pass_wait-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/step01_pass_wait-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Step01PassWait.Step01PassWaitStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
