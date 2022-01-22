1111086053a2593204dc513c732da4f9983f454e
Hassan Ali Khan <73381969+hassan-ak@users.noreply.github.com>
2022-01-22 18:34:59 +0500

    Update step02_choice_part1-stack.ts

--- step20_step_functions/step02_choice_part1/lib/step02_choice_part1-stack.ts
+++ step20_step_functions/step02_choice_part1/lib/step02_choice_part1-stack.ts
@@ -1,15 +1,74 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as ddb from '@aws-cdk/aws-dynamodb';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as stepFunctionTasks from '@aws-cdk/aws-stepfunctions-tasks';
+import * as stepFunctions from '@aws-cdk/aws-stepfunctions';
 
 export class Step02ChoicePart1Stack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // created a dynamodb Table
+    const DynamoTable = new ddb.Table(this, 'DynamoTable', {
+      partitionKey: {
+        name: 'id',
+        type: ddb.AttributeType.STRING,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'Step02ChoicePart1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // this function adds data to the dynamoDB table
+    const addData = new lambda.Function(this, 'addData', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      code: lambda.Code.fromAsset('lambda'),
+      handler: 'addData.handler',
+    });
+    // giving access to the lambda function to do operations on the dynamodb table
+    DynamoTable.grantFullAccess(addData);
+    addData.addEnvironment('DynamoTable', DynamoTable.tableName);
+
+    // creating steps for the step function
+    const lambdafn = new stepFunctionTasks.LambdaInvoke(
+      this,
+      'Invoke addData lambda',
+      {
+        lambdaFunction: addData,
+      }
+    );
+
+    // Reaching a Succeed state terminates the state machine execution with a succesful status.
+    const success = new stepFunctions.Succeed(this, 'We did it!');
+    // Reaching a Fail state terminates the state machine execution with a failure status.
+    const jobFailed = new stepFunctions.Fail(this, 'Job Failed', {
+      cause: 'Lambda Job Failed',
+      error: 'could not add data to the dynamoDb',
+    });
+
+    // Here we are putting a condition to choose our next state. If the last state (lambdafn) returns {operationSuccessful: true}
+    // then we end our state machine with a success state otherwise with a fail state
+    const choice = new stepFunctions.Choice(this, 'operation successful?');
+    choice.when(
+      stepFunctions.Condition.booleanEquals(
+        '$.Payload.operationSuccessful',
+        true
+      ),
+      success
+    );
+    choice.when(
+      stepFunctions.Condition.booleanEquals(
+        '$.Payload.operationSuccessful',
+        false
+      ),
+      jobFailed
+    );
+
+    // creating chain to define the sequence of execution
+    const chain = stepFunctions.Chain.start(lambdafn).next(choice);
+    // create a state machine
+    new stepFunctions.StateMachine(this, 'choiceStateMachine', {
+      definition: chain,
+    });
   }
 }
+
+//   }
+// }
